6e0c737ad773d8a7e0846c586c99267166e01a87
Andrew Green <agreen@ebi.ac.uk>
2025-01-07 11:55:47 +0000

    Fix bad quotations in fstring

--- src/mirna_curator/main.py
+++ src/mirna_curator/main.py
@@ -130,7 +130,7 @@ def main(config: Optional[str] = None,
             break
         article = fetch.article(row["PMCID"])
         curation_result = graph.execute_graph(llm, article, row["rna_id"], prompt_data)
-        logger.info(f"RNA ID: {row.rna_id} - Curation Result: {curation_result}")
+        logger.info(f"RNA ID: {row['rna_id']} in {row['PMCID']} - Curation Result: {curation_result}")
         curation_output.append({"PMCID": row["PMCID"], "rna_id": row["rna_id"], "curation_result": curation_result})
   
 
