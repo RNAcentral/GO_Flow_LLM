9504d88ae72a45aeb3af6831f2227de1b20695f7
Andrew Green <agreen@ebi.ac.uk>
2025-01-09 09:27:47 +0000

    Write job script

---
+++ job_script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Submit this script with: sbatch thefilename
+#For more details about each parameter, please check SLURM sbatch documentation https://slurm.schedmd.com/sbatch.html
+
+#SBATCH --time=2:00:00   # walltime
+#SBATCH --ntasks=1   # number of tasks
+#SBATCH --cpus-per-task=24   # number of CPUs Per Task i.e if your code is multi-threaded
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --gres=gpu:1
+#SBATCH --mem=32G   # memory per node
+#SBATCH -J "go_flow_llm"   # job name
+#SBATCH -o "out_gfllm"   # job output file
+#SBATCH -e "err_gfllm"   # job error file
+#SBATCH --mail-user=agreen@ebi.ac.uk   # email address
+#SBATCH --mail-type=END
+
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+
+pyenv activate mirna_curator
+
+python src/mirna_curator/main.py --config curation_config_qwen72B.json
+
