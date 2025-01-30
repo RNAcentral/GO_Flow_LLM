f383753bd1f83716cbd6517ecbfd1a981edea4a3
Andrew Green <agreen@ebi.ac.uk>
2025-01-30 15:46:42 +0000

    Add a quick wrapper for the epmc annotations API

---
+++ src/mirna_curator/apis/epmc.py
@@ -0,0 +1,21 @@
+import requests
+from typing import List
+annotations_endpoint_url = "https://www.ebi.ac.uk/europepmc/annotations_api/annotationsByArticleIds?articleIds=PMC:{pmcid}&type=Gene_Proteins&provider=Europe PMC"
+
+def get_gene_name_annotations(pmcid: str) -> List[str]:
+    """
+    Call the EuropePMC API to get gene annotations for a paper. Then 
+    filter the result to get a sorted, unique list of gene names
+
+    This can then be given to guidance to select from
+    """
+    res = requests.get(annotations_endpoint_url.format(pmcid=pmcid))
+
+    res.raise_for_status()
+
+    annotations = res.json()[0]['annotations']
+
+    gene_names = [a['tags'][0]['name'] for a in annotations]
+
+    gene_names = sorted(list(set(gene_names)))
+    return gene_names
