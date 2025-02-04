66572e04dc4c288aed7eb7f459ca7c050d06cc41
Andrew Green <agreen@ebi.ac.uk>
2025-02-04 13:27:14 +0000

    Update job script with better time estimate

--- job_script.sh
+++ job_script.sh
@@ -3,15 +3,15 @@
 #Submit this script with: sbatch thefilename
 #For more details about each parameter, please check SLURM sbatch documentation https://slurm.schedmd.com/sbatch.html
 
-#SBATCH --time=10:00:00   # walltime
+#SBATCH --time=7:00:00   # walltime
 #SBATCH --ntasks=1   # number of tasks
 #SBATCH --cpus-per-task=24   # number of CPUs Per Task i.e if your code is multi-threaded
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --gres=gpu:a100:1
 #SBATCH --mem=48G   # memory per node
 #SBATCH -J "go_flow_llm"   # job name
-#SBATCH -o "out_gfllm_disease"   # job output file
-#SBATCH -e "err_gfllm_disease"   # job error file
+#SBATCH -o "out_gfllm"   # job output file
+#SBATCH -e "err_gfllm"   # job error file
 #SBATCH --mail-user=agreen@ebi.ac.uk   # email address
 #SBATCH --mail-type=END
 
