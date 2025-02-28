0bdb8e26f5da174de20050f71ce170c90f81ee3c
Andrew Green <agreen@ebi.ac.uk>
2025-02-28 13:28:40 +0000

    Tool use node implementation
    Uses a ReAct agent within the node to select the most appropriate tool to use, use it and then interpret the results.
    
    Working ok in simple tests, but not tested in anger yet

--- src/mirna_curator/llm_functions/conditions.py
+++ src/mirna_curator/llm_functions/conditions.py
@@ -10,6 +10,7 @@ from guidance import gen, select, system, user, assistant, with_temperature, sub
 from mirna_curator.llm_functions.evidence import extract_evidence
 from mirna_curator.apis import epmc
 from mirna_curator.model.llm import STOP_TOKENS
+from mirna_curator.llm_functions.tools import safe_import
 import typing as ty
 
 import logging
@@ -42,6 +43,104 @@ def prompted_flowchart_step_bool(
         else:
             llm += "\n\n"
         llm += f"Question: {step_prompt}\nRestrict your considerations to {rna_id} if there are multiple RNAs mentioned\n"
+
+        llm += "Explain your reasoning step-by-step. Be concise\n"
+            
+    logger.info(f"LLM input tokens: {llm.engine.metrics.engine_input_tokens}")
+    logger.info(f"LLM generated tokens: {llm.engine.metrics.engine_output_tokens}")
+    logger.info(
+        f"LLM total tokens: {llm.engine.metrics.engine_input_tokens + llm.engine.metrics.engine_output_tokens}"
+    )
+    with assistant():
+        llm += (
+            with_temperature(
+                gen(
+                    "reasoning",
+                    max_tokens=512,
+                    stop=STOP_TOKENS,
+                ),
+                temperature_reasoning,
+            )
+            + "\n"
+        )
+
+    with assistant():
+        llm += f"The final answer, based on my reasoning above is: " + with_temperature(
+            select(["yes", "no"], name="answer"), temperature_selection
+        )
+
+    llm += extract_evidence(article_text, mode="recursive-paragraph")
+
+    return llm
+
+
+@guidance
+def prompted_flowchart_step_tool(
+    llm: guidance.models.Model,
+    article_text: str,
+    load_article_text: bool,
+    step_prompt: str,
+    rna_id: str,
+    tools: ty.Optional[ty.List[str]] = [],
+    temperature_reasoning: ty.Optional[float] = 0.6,
+    temperature_selection: ty.Optional[float] = 0.4,
+) -> guidance.models.Model:
+    """
+    Use the given prompt on the article text to answer a yes/no question,
+    returning a boolean
+
+    Args:
+        lm: guidance.models.Model: A guidance model that's ready to go
+        article_text: str: The article text we need to work on
+        load_article_text: bool : Flag for whether we are going to load the text into the context
+        step_prompt: str: The prompt read from the flowchart json file
+        rna_id: str: The RNA id we are working on
+        tools: ty.Optional[ty.List[str]] = []: A list of tools for the LLM to use
+        temperature_reasoning: ty.Optional[float] = 0.6: The reasoning temperature (0.6 is R1 reccomended)
+        temperature_selection: ty.Optional[float] = 0.4: The yes/no selection temperature
+    """
+
+    ## build the tool description string.
+    tool_dict = safe_import(tools)
+    tools_string = ("To help me answer this question, I have access to some tools to look"
+                    " up some information. The tools are described here:\n"
+                    "===========================\n"
+    )
+
+    for tool_name, tool in tool_dict.items():
+        tools_string += f"Name: {tool_name}\n+++++++++++\n"
+        tools_string += f"Description:\n{tool.__doc__}"
+        tools_string += "\n+++++++++++\n"
+    tools_string += "===========================\n"
+    print(tools_string)
+    tools.append("finish")
+    with user():
+        if load_article_text:
+            logger.info(
+                f"Appending {len(llm.engine.tokenizer.encode(article_text.encode('utf-8')))} tokens (internal node)"
+            )
+            llm += f"You will be asked a yes/no question. The answer could be in following text, or it could be in some text you have already seen: \n{article_text}\n\n"
+        else:
+            llm += "\n\n"
+
+        llm += f"Question: {step_prompt}\n"
+    
+    ## Make a tiny little ReAct agent loop
+    i = 0
+    max_steps = 5
+    with assistant():
+        llm += tools_string
+        while True:
+            llm += f"Thought {i}: " + gen(suffix='\n')
+            llm += f"Act {i}: " + select(tools, name='act')
+            llm += "[" + gen(name="arg", stop=']') + "]\n" 
+            if llm['act'] == 'finish' or i > max_steps:
+                break
+            else:
+                tool_output = tool_dict[llm['act']](llm['arg'])
+                llm += f"Observation {i}: {tool_output}\n"
+            i += 1
+        # Restrict your considerations to {rna_id} if there are multiple RNAs mentioned\n"
 
         llm += "Explain your reasoning step-by-step. Be concise\n"
             
