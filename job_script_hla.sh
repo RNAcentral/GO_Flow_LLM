3dcecaf65628795ebb2fd9357f162839248570db
Andrew Green <agreen@ebi.ac.uk>
2025-03-09 17:29:41 +0000

    Try a huge model - mistral large at 123B

--- job_script_hla.sh
+++ job_script_hla.sh
@@ -3,11 +3,11 @@
 #Submit this script with: sbatch thefilename
 #For more details about each parameter, please check SLURM sbatch documentation https://slurm.schedmd.com/sbatch.html
 
-#SBATCH --time=1:00:00   # walltime
+#SBATCH --time=7:00:00   # walltime
 #SBATCH --ntasks=1   # number of tasks
 #SBATCH --cpus-per-task=24   # number of CPUs Per Task i.e if your code is multi-threaded
 #SBATCH --nodes=1   # number of nodes
-#SBATCH --gres=gpu:a100:1
+#SBATCH --gres=gpu:a100:2
 #SBATCH --mem=48G   # memory per node
 #SBATCH -J "go_flow_llm_updated_fc"   # job name
 #SBATCH -o "out_gfllm_updated_fc"   # job output file
@@ -21,7 +21,7 @@ source ~/.pyenv_setup
 export HF_HUB_ENABLE_HF_TRANSFER=1
 pyenv activate mirna-curator
 
-python src/mirna_curator/main.py --config configs/curation_config_QwQ_HLA.json
+# python src/mirna_curator/main.py --config configs/curation_config_QwQ_HLA.json
 
-# python src/mirna_curator/main.py --config configs/curation_config_R1-Distill-Llama70B.json
+python src/mirna_curator/main.py --config configs/curation_config_mistral_large.json
 
