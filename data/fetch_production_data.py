4f80d3c5742a15f01c15c1efaa29bf8fd0342ad9
Andrew Green <agreen@ebi.ac.uk>
2025-03-31 11:50:08 +0100

    Split input data 4 ways to allow multiprocessing

--- data/fetch_production_data.py
+++ data/fetch_production_data.py
@@ -21,8 +21,16 @@ having cardinality(array_agg(lsdb.job_id)) = 1
 """
 
 prod_data = pl.read_database_uri(query, os.getenv("PGDATABASE"))
-prod_data = prod_data.rename({"pmcid": "PMCID"})
+prod_data = prod_data.rename({"pmcid": "PMCID"}).with_row_index()
 
 timestamp = datetime.today().strftime("%Y-%m-%d")
 
+print(prod_data.height)
 prod_data.write_parquet(f"production_test_data_{timestamp}.parquet")
+
+n_splits = 4
+n_per_split = prod_data.height // n_splits
+splits = [prod_data.filter(pl.col("index").is_between((i-1)*n_per_split, i*n_per_split)) for i in range(1,n_splits+1)]
+
+for n, s in enumerate(splits):
+    s.write_parquet(f"production_test_data_{timestamp}_split_{n}.parquet")
