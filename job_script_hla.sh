efecd5a5ff21a4d14445ea7fe5d7a741acedb175
Andrew Green <agreen@ebi.ac.uk>
2025-04-04 14:53:55 +0100

    Switch job script config path back to development set and reduce time

--- job_script_hla.sh
+++ job_script_hla.sh
@@ -3,7 +3,7 @@
 #Submit this script with: sbatch thefilename
 #For more details about each parameter, please check SLURM sbatch documentation https://slurm.schedmd.com/sbatch.html
 
-#SBATCH --time=96:00:00   # walltime
+#SBATCH --time=9:00:00   # walltime
 #SBATCH --ntasks=1   # number of tasks
 #SBATCH --cpus-per-task=24   # number of CPUs Per Task i.e if your code is multi-threaded
 #SBATCH --nodes=1   # number of nodes
@@ -21,7 +21,7 @@ source ~/.pyenv_setup
 export HF_HUB_ENABLE_HF_TRANSFER=1
 pyenv activate mirna-curator
 
-# python src/mirna_curator/main.py --config configs/curation_config_QwQ_HLA.json
+python src/mirna_curator/main.py --config configs/curation_config_QwQ_HLA.json
 
-python src/mirna_curator/main.py --config configs/curation_config_QwQ_prod.json
+# python src/mirna_curator/main.py --config configs/curation_config_QwQ_prod.json
 
