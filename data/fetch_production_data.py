b5222732168ada0528107694b8c9404a67af1136
Andrew Green <agreen@ebi.ac.uk>
2025-05-21 12:20:26 +0100

    Optimized the production data candidate paper selection query

--- data/fetch_production_data.py
+++ data/fetch_production_data.py
@@ -3,23 +3,23 @@ import os
 from datetime import datetime
 
 query = """
-select
-
-pmcid as PMCID,
-(array_agg(lsdb.job_id))[1] as rna_id
-
-from litscan_result lsr
-left join (select distinct lsdb.job_id, lsdb.name from litscan_database lsdb) lsdb
-	on lsdb.job_id = lsr.job_id
-join litscan_job lsj 
-	on lsj.job_id = lsdb.job_id
-where name in ('mirbase', 'mirgenedb')
-and hit_count > 0
-and retracted = false
-and type = 'Research article'
-
-group by pmcid
-having cardinality(array_agg(lsdb.job_id)) = 1
+SELECT
+    lsr.pmcid AS PMCID,
+    (ARRAY_AGG(lsdb.job_id))[1] AS rna_id
+FROM litscan_result lsr
+JOIN litscan_database lsdb ON lsdb.job_id = lsr.job_id
+JOIN litscan_article lsa ON lsa.pmcid = lsr.pmcid
+WHERE lsdb.name IN ('mirbase', 'mirgenedb')
+    AND lsa.retracted = FALSE
+    AND lsa.type = 'Research article'
+    AND EXISTS (
+        SELECT 1
+        FROM litscan_job lsj
+        WHERE lsj.job_id = lsdb.job_id
+        AND lsj.hit_count > 0
+    )
+GROUP BY lsr.pmcid
+HAVING COUNT(DISTINCT lsdb.job_id) = 1
 """
 
 prod_data = pl.read_database_uri(query, os.getenv("PGDATABASE"))
